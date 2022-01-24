9da201462ce89fc7ec5b430b72318b958b389570
blaz <blaz@shannon>
2022-01-24 12:21:26 -0300

    Move to CMake, change directory structure and delete repeated Huffman code

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
 CPP=clang++-11 # Debug
 #CPP=g++
-#FLAGS=-O9 -Wall -DNDEBUG -I libcds/includes/ 
-FLAGS= -Wall -g -DNDEBUG -fsanitize=integer -fsanitize=undefined -fsanitize=address -I libcds/includes/ # Debug
+#FLAGS=-O9 -Wall -DNDEBUG -I libcds/includes/ # Original
+#FLAGS=-O0 -g3 -Wall -I libcds/includes/  # Debug gcc
+#FLAGS=-O0 -pg -Wall -I libcds/includes/  # Debug + prof gcc
+#FLAGS=-O3 -g0 -Wall -I libcds/includes/  # Release
+FLAGS= -Wall -g -DNDEBUG -fsanitize=integer -fsanitize=undefined -fsanitize=address -I libcds/includes/ # Debug clang
 LIB=libcds/lib/libcds.a
 
 OBJECTS_CODER=utils/Coder/StatCoder.o utils/Coder/DecodingTableBuilder.o utils/Coder/DecodingTable.o utils/Coder/DecodingTree.o utils/Coder/BinaryNode.o
@@ -13,10 +16,10 @@ OBJECTS_HASH=Hash/Hash.o Hash/HashDAC.o Hash/Hashdh.o Hash/HashBdh.o Hash/HashBB
 OBJECTS_HUFFMAN=Huffman/huff.o Huffman/Huffman.o
 OBJECTS_FMINDEX=FMIndex/SuffixArray.o FMIndex/SSA.o
 OBJECTS_XBW=XBW/TrieNode.o XBW/XBW.o  
-OBJECTS=$(OBJECTS_UTILS) $(OBJECTS_HUTUCKER) $(OBJECTS_HUFFMAN) $(OBJECTS_REPAIR) $(OBJECTS_HASH) $(OBJECTS_XBW) $(OBJECTS_FMINDEX) StringDictionary.o StringDictionaryPFC.o StringDictionaryHHTFC.o StringDictionaryRPHTFC.o
-EXES=Build.o Test.o
+OBJECTS=$(OBJECTS_UTILS) $(OBJECTS_HUTUCKER) $(OBJECTS_HUFFMAN) $(OBJECTS_REPAIR) $(OBJECTS_HASH) $(OBJECTS_XBW) $(OBJECTS_FMINDEX) StringDictionary.o StringDictionaryPFC.o StringDictionaryHHTFC.o StringDictionaryRPHTFC.o StringDictionaryFMINDEX.o
+EXES=Build.o Test.o Test-single.o Test-substr.o
 
-BIN=Build Test
+BIN=Build Test Test-single Test-substr
 
 %.o: %.cpp
 	@echo " [C++] Compiling $<"
@@ -31,7 +34,12 @@ Build:
 	
 Test:	
 	$(CPP) $(FLAGS) -o Test Test.o $(OBJECTS) ${LIB}
- 
+
+Test-single:
+	$(CPP) $(FLAGS) -o Test-single Test-single.o $(OBJECTS) ${LIB}
+
+Test-substr:
+	$(CPP) $(FLAGS) -o Test-substr Test-substr.o $(OBJECTS) ${LIB}
 
 clean:
 	@echo " [CLN] Removing object files"
---
+++ src/main/test-single.cpp
@@ -0,0 +1,148 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <chrono>
+#include "dicts/StringDictionary.h"
+
+using namespace std;
+
+const char BAR_CHAR = '|';
+const int BAR_WIDTH = 60;
+
+string prog;
+
+map <string, uint32_t> ext_map = {
+    {"pfc", PFC},
+    {"hhtfc", HHTFC},
+    {"rphtfc", RPHTFC},
+    {"fmi", FMINDEX},
+    };
+
+void printUsage(){
+    cerr << "Usage: " << endl;
+    cerr << prog << " <compressed-dict> <uncompressed-dict>" << endl;
+    exit(1);
+}
+
+void printBar(int progress){
+    int left = progress*BAR_WIDTH/100;
+    int right = BAR_WIDTH - left;
+    if(progress < 10) cerr << " ";
+    cerr << '\r' << progress << "% [";
+    for(int i=0; i<left; i++) cerr << BAR_CHAR;
+    for(int i=0; i<right; i++) cerr << " ";
+    cerr << "]";
+    cerr.flush();
+}
+
+uint64_t getStreamSize(ifstream &st){
+    st.seekg(0, st.end);
+    uint64_t ret = st.tellg();
+    st.seekg(0, st.beg);
+    return ret;
+}
+
+int main(int argc, char *argv[]){
+    prog = argv[0];
+    if(argc != 3){
+        printUsage();
+    }
+
+    /* Parse input and create streams */
+    string compDictFilename = argv[1];
+    string rawDictFilename = argv[2];
+
+    ifstream rawDictStream(rawDictFilename);
+    ifstream compDictStream(compDictFilename);
+    
+    if(!rawDictStream.good()){
+        cout << "Could not find or open " << rawDictFilename << ".\n";
+        exit(1);
+    } 
+    if(!compDictStream.good()){
+        cout << "Could not find or open " << compDictFilename << ".\n";
+        exit(1);
+    } 
+
+    /* Create dictionary object */
+    string ext = compDictFilename.substr(compDictFilename.find_last_of('.')+1);
+    if(ext_map.find(ext) == ext_map.end()){
+        cout << "Invalid extension for the compressed dictionary filename.\n";
+        exit(1);
+    }
+	unique_ptr<StringDictionary> dict = StringDictionary::load(compDictStream, ext_map[ext]);
+
+    /* Testing */
+    using namespace std::chrono;
+
+    steady_clock::duration locTimeDur(0);
+    steady_clock::duration extTimeDur(0);
+
+    uint64_t readStrings=0;
+    uint64_t readBytes=0;
+    uint64_t totalBytes = getStreamSize(rawDictStream);
+
+    string cur;
+
+    int progress=0;
+    steady_clock::time_point testStart = steady_clock::now();
+    while(getline(rawDictStream, cur, '\0')){
+        // Some declarations
+        steady_clock::time_point t0, t1;
+        uint64_t locId; // located id
+        uchar *extractedPtr; // extracted string
+        uint_fast32_t strLen = cur.size();
+        uchar *curPtr = (uchar*)&cur[0];
+
+        // Run locate
+        t0 = steady_clock::now();
+        locId = dict->locate(curPtr, strLen);
+        t1 = steady_clock::now();
+        locTimeDur += t1-t0;
+
+        // Run extract
+        t0 = steady_clock::now();
+        extractedPtr = dict->extract(locId, &strLen);
+        t1 = steady_clock::now();
+        extTimeDur += t1-t0;
+        // Check extract result
+        if(strLen != cur.size()){
+            cout << "Extract error: returned size is not the expected.\n";
+            cout << "\tReturned size: " << strLen << '\n';
+            cout << "\tExpected size: " << cur.size() << '\n';
+            cout << "String is " << cur << '\n';
+            cout << "ID is " << locId << '\n';
+            exit(1);
+        }
+        if(cur != (char*)extractedPtr){
+            cout << "Extract error: extracted string is not the expected.\n";
+            cout << "\tExtracted string: " << string((char*)extractedPtr, strLen) << '\n';
+            cout << "\tExpected string: " << cur << '\n';
+            cout << "ID is " << locId << '\n';
+            exit(1);
+        }
+        
+        readStrings++;
+        readBytes += cur.size()+1;
+        if(int(100*readBytes/totalBytes) > progress){
+            progress = 100*readBytes/totalBytes;
+            printBar(progress);
+        }
+    }
+    cerr << '\n';
+    steady_clock::time_point testEnd = steady_clock::now();
+    
+
+    double locTime = duration_cast<microseconds>(locTimeDur).count();
+    double extTime = duration_cast<microseconds>(extTimeDur).count();
+
+    cout << "Test successful:\n";
+    cout << "\tTested strings: " << readStrings << '\n';
+    cout << "\tTotal test time: " << duration_cast<seconds>(testEnd-testStart).count() << " [seconds]\n";
+    cout << "\tTotal locate time: " << locTime << " [microsec]\n";
+    cout << "\tMean locate time: " << locTime/readStrings << " [microsec]\n";
+    cout << "\tTotal extract time: " << extTime << " [microsec]\n";
+    cout << "\tMean extract time: " << extTime/readStrings << " [microsec]\n";
+    return 0;
+}
